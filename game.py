0e4f72e595b1c3847826d6b025a6a6f301097d53
Virana Chavelin <victorinus.waradana@gmail.com>
2026-01-19 06:06:56 +0000

    Game v1.0

---
+++ game.py
@@ -0,0 +1,11 @@
+#Aplikasi game simpel number
+
+secret_number = 67
+guess_number = int(input("Masukkan angka tebakan: "))
+
+while guess_number != secret_number:
+    print("Jawaban salah.")
+    guess_number = int(input("Masukkan angka lagi: "))
+
+print('Tebakan anda benar. Selamat!')
+print('Kode ini saya buat di codespace.')
