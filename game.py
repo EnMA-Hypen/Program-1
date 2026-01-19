ef55adff2babed5867add4f55cd8d3cc49aa9a17
Virana Chavelin <victorinus.waradana@gmail.com>
2026-01-19 06:10:24 +0000

    Game v1.1

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ guess_number = int(input("Masukkan angka tebakan: "))
 
 while guess_number != secret_number:
     print("Jawaban salah.")
+    print('Anda terjebak dalam loop.')
     guess_number = int(input("Masukkan angka lagi: "))
 
 print('Tebakan anda benar. Selamat!')
